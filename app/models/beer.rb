38e8d7b0adf14ca74625a802de16236db64b617e
Mike Brewer <mikey.brew@gmail.com>
2014-12-10 19:10:11 -0600

    Version finalized
    Adding some additional gems for bonus points next

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -2,7 +2,10 @@ class Beer < ActiveRecord::Base
   belongs_to(:user, { :class_name => "User", :foreign_key => "user_id"})
   belongs_to(:staticbeer, {:class_name => "StaticBeer", :foreign_key => "beer_type"})
 
+  validates( :beer_type, :presence => true)
   validates( :beer_name, :presence => true)
+  validates( :star_rating, :presence => true)
+  validates( :fav_save, :presence => true)
   validates( :num_consumed, :presence => true, numericality: { only_integer: true })
   validates( :user_id, :presence => true)
   validates( :user, :presence => true)
